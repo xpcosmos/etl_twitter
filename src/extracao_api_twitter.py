e4848c1500554e46d6eb72c6c85071e3891d1914
xpcosmos <mikeias.d.s.o@gmail.com>
2023-02-12 12:19:13 -0300

    criacao de header

--- src/extracao_api_twitter.py
+++ src/extracao_api_twitter.py
@@ -1,3 +1,18 @@
 from datetime import datetime, timedelta
+import os
 
-end_tine = datetime.now()
+
+TIMESTAMP_FORMAT = "%Y-%m-%dT%H:%M:%S.00Z"
+end_time = datetime.now()
+start_time = (datetime.now() + timedelta(-1).date())
+
+query = 'data science'
+
+tweet_fields = "tweet.fields=author_id,conversation_id,created_at,id,in_reply_to_user_id,public_metrics,lang,text"
+user_fields = "expansions=author_id&user.fields=id,name,username,created_at"
+
+url_raw = f"https://api.twitter.com/2/tweets/search/recent?query={query}&{tweet_fields}&{user_fields}&{start_time}&{end_time}"
+
+bearer_token = os.environ.get("BEARER_TOKEN")
+headers = {'Authorization':f'Bearer {bearer_token}'}
+
